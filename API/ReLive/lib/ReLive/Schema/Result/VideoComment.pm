f0a2550fc2d9a25ca4bbc59590f9cbca7bda19b6
Trevor Walker <trevor@fudgedesign.co.uk>
2012-12-14 18:07:27 +0000

    piu piu piu coocoo bau bau miao miao

--- API/ReLive/lib/ReLive/Schema/Result/VideoComment.pm
+++ API/ReLive/lib/ReLive/Schema/Result/VideoComment.pm
@@ -8,6 +8,8 @@ use base 'DBIx::Class::Core';
 __PACKAGE__->load_components("InflateColumn::DateTime");
 __PACKAGE__->table("comments");
 __PACKAGE__->add_columns(
+    "id",
+    {data_type => "integer", is_nullable => 0, is_auto_increment => 1},
     "user_id",
     {data_type => "char", is_nullable => 0, size => 255},
     "video_id",
@@ -20,7 +22,7 @@ __PACKAGE__->add_columns(
     {data_type => "integer", is_nullable => 0},
 );
 
-__PACKAGE__->set_primary_key("user_id", "video_id", "created_at");
+__PACKAGE__->set_primary_key("id");
 
 __PACKAGE__->belongs_to( user => "ReLive::Schema::Result::User", "user_id" );
 __PACKAGE__->belongs_to( video => "ReLive::Schema::Result::Video", "video_id" );
